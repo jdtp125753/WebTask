07789d6e367e70e901cfddc1224142c414d13554
jdtp125753 <jhojan1806@gmail.com>
2023-05-26 17:06:27 -0500

    add subTask, ok

--- BullBionicTodo/src/app/Components/ListPendingTasks/all-pending/all-pending.component.ts
+++ BullBionicTodo/src/app/Components/ListPendingTasks/all-pending/all-pending.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { PendingService } from '../../../Sevice/pending.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-all-pending',
@@ -8,12 +9,50 @@ import { PendingService } from '../../../Sevice/pending.service';
 })
 export class AllPendingComponent {
   @Input() pending: any;
-  subT: any[]; 
+  Task: any; 
+  subTodo: any[]; 
   idSub: number; 
+  task: FormGroup;
   subTasking: boolean = false;
   editTask: boolean = false; 
-  constructor(private pendinServi: PendingService) {
+  addSubTask: boolean = false; 
+  
+  constructor(private servi: PendingService) {
+    this.task = new FormGroup({
+      title: new FormControl(''),
+      description: new FormControl(''),
+      addSub: new FormControl(''), 
+      stade: new FormControl(''),
+      deadLine: new FormControl('')
+    })
+  }
 
+  ngOnInit(): void {
+    
+    this.Task = this.pending; 
+    this.subTodo = this.Task.subTasks;
+    this.Task.subTasks = this.subTodo;
+  }
+  
+  saveSubTodo():void{
+    let inputvalue = this.task.controls['addSub'].value; 
+    if(inputvalue.trim() === ''){
+      alert('Agrega una subtarea antes de guardar');
+    }else{
+      let n1: number = this.subTodo.length;
+      let subts: any = {id: n1 + 1, title: this.task.value['addSub']}
+      this.subTodo.push(subts); 
+      this.servi.updateTaskPending( this.Task.id, this.Task).subscribe(
+        (e:any) => console.log('Actualizando Tarea')
+      );
+  
+      this.addSubTask =false;
+    }
+  }
+
+  addTodo():boolean{
+    this.task.controls['addSub'].setValue('');
+    return this.addSubTask = !this.addSubTask; 
   }
   idSubTask():number {
     return this.idSub = this.pending.id;
@@ -27,5 +66,4 @@ export class AllPendingComponent {
     return this.subTasking;
   }
   
-  ngOnInit(): void { }
 }
